747165de1c5bf20e2f9b8e05e80a15fb1b4b9e89
SpiralP <spiral.palmer@gmail.com>
2020-04-13 12:00:40 -0700

    Update lua files to support x64 (#6)
    * Update lua files to support x64
    * Update require.lua
    * Update unrequire.lua

--- includes/modules/unrequire.lua
+++ includes/modules/unrequire.lua
@@ -3,10 +3,28 @@ local package_loaded = package.loaded
 local _R = debug.getregistry()
 local _LOADLIB = _R._LOADLIB
 
-local prefix = SERVER and "gmsv" or "gmcl"
-local suffix = system.IsWindows() and "win32" or (system.IsLinux() and "linux" or "osx")
-local sep = system.IsWindows() and "\\" or "/"
-local fmt = string.format("^LOADLIB: .+%sgarrysmod%slua%sbin%s%s_%%s_%s.dll$", sep, sep, sep, sep, prefix, suffix)
+local is_windows = system.IsWindows()
+local is_linux = system.IsLinux()
+local is_osx = system.IsOSX()
+local is_x64 = jit.arch == "x64"
+local separator = is_windows and "\\" or "/"
+
+local dll_prefix = CLIENT and "gmcl" or "gmsv"
+local dll_suffix = assert(
+	(is_windows and (is_x64 and "win64" or "win32")) or
+	(is_linux and (is_x64 and "linux64" or "linux")) or
+	(is_osx and (is_x64 and "osx64" or "osx"))
+)
+
+local fmt = string.format(
+	"^LOADLIB: .+%sgarrysmod%slua%sbin%s%s_%%s_%s.dll$",
+	separator,
+	separator,
+	separator,
+	separator,
+	dll_prefix,
+	dll_suffix
+)
 
 function unrequire(name)
 	_MODULES[name] = nil
